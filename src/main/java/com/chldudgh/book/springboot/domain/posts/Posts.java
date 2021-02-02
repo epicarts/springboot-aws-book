1678dd1ac52bd2277f561a7a04b12453cce46bf5
YoungHo Choi <0505zxc@gmail.com>
2021-02-02 23:55:41 +0900

    PostsRepository 구현 및 테스트 코드 작성

---
+++ src/main/java/com/chldudgh/book/springboot/domain/posts/Posts.java
@@ -0,0 +1,32 @@
+package com.chldudgh.book.springboot.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Posts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author; //없어도 기본값으로 추가됨
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
