e880388369f0e5932d9c785f9f686693f447cf69
YoungHo Choi <0505zxc@gmail.com>
2021-02-09 03:09:12 +0900

    LoginUser 어노테이션 작성

--- src/main/java/com/chldudgh/book/springboot/web/indexController.java
+++ src/main/java/com/chldudgh/book/springboot/web/indexController.java
@@ -1,5 +1,6 @@
 package com.chldudgh.book.springboot.web;
 
+import com.chldudgh.book.springboot.config.auth.LoginUser;
 import com.chldudgh.book.springboot.config.auth.dto.SessionUser;
 import com.chldudgh.book.springboot.domain.posts.PostsRepository;
 import com.chldudgh.book.springboot.service.posts.PostsService;
@@ -21,9 +22,8 @@ public class indexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if (user != null) {
             model.addAttribute("userName", user.getClass());
